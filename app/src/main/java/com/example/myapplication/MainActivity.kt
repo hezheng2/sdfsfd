3a2db1766c91a909719d70ffbf786196d0a9e53c
student <werwrew>
2021-03-12 14:39:38 +0800

    thrid

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,12 +6,10 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        var txv:TextView= findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var txv:TextView= findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
     }
 }
