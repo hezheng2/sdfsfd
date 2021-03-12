016c688ebf8c06ed4fa0ee6fa64abe2415d93538
student <werwrew>
2021-03-12 15:48:39 +0800

    fourth

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,10 +6,17 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var txv:TextView= findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = “Hello tcyang”
+
+        TestMaster()
+    }
+
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
     }
 }
